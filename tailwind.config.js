3523b1bfe73855ef8d5380e967e5546d12450efd
davidjirca <davidgjirca@gmail.com>
2022-05-14 01:59:43 +0300

    first vue app + tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+}
